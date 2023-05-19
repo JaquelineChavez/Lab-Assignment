d540990357510a951749db2ae379f9e1f4ec26de
Jaqueline Chavez <jacquichavez2001@gmail.com>
2023-05-19 01:25:57 -0700

    Update python script.py

--- python script.py
+++ python script.py
@@ -2,3 +2,7 @@ def StudentName():
     userInput = input('Please enter your full name:')
     print(userInput)
 StudentName()
+
+def function(*schoolYear)
+    print('They student is in their' + schoolYear[4]) 
+function('1st', '2nd', '3rd', '4th')
